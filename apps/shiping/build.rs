e49cb63666c0ffcb14704376b3d2302b820c9f10
king <dripai@users.noreply.github.com>
2026-07-20 13:08:15 +0800

    refactor: align preferences settings and status

--- apps/shiping/build.rs
+++ apps/shiping/build.rs
@@ -1,4 +1,8 @@
 fn main() {
-    let config = slint_build::CompilerConfiguration::new().with_style("fluent-dark".into());
+    let library_paths =
+        std::collections::HashMap::from([("shi-ui".to_owned(), shi_ui::slint_library_path())]);
+    let config = slint_build::CompilerConfiguration::new()
+        .with_library_paths(library_paths)
+        .with_style("fluent-dark".into());
     slint_build::compile_with_config("ui/app.slint", config).expect("compile ShiPing Slint UI");
 }
